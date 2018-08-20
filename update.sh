c163cb3a4f5a34cedce07e2c9faa50a52df9f136
Jesper Flodin <jesper@flodin.fi>
2018-08-20 22:50:06 +0200

    Added oslog

--- update.sh
+++ update.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 #clean up
-find . -name ‘*.DS_Store’ -type f -delete
+find ~/Documents/GitHub/jesperflodin1.github.io/ -name ‘*.DS_Store’ -type f -delete
+find ~/Documents/iOS-dev/ -name ‘*.DS_Store’ -type f -delete
+find ~/Documents/Projects/Tweaks/ -name ‘*.DS_Store’ -type f -delete
+
 #uncomment these 2 lines if you don't want a downgrade option in Cydia 
 #find ./debs -type f -name '*.deb' -delete
 #rm -r Packages.bz2
@@ -10,8 +13,10 @@ find . -name ‘*.DS_Store’ -type f -delete
 #format example
 # dpkg-deb --bZlzma Projects/<projectname> <output folder>
 #examples
-dpkg-deb -bZgzip ~/Documents/iOS-dev/packages/SB.JF.1 debs
+dpkg-deb -bZgzip ~/Documents/iOS-dev/packages/SB.JF.1 ~/Documents/GitHub/jesperflodin1.github.io/debs
+dpkg-deb -bZgzip ~/Documents/iOS-dev/packages/net.limneos.oslog ~/Documents/GitHub/jesperflodin1.github.io/debs
+cp ~/Documents/Projects/Tweaks/tonehelper/packages/*.deb ~/Documents/GitHub/jesperflodin1.github.io/debs
 
 #packages
-dpkg-scanpackages -m ./debs > Packages
-bzip2 -fks Packages
+dpkg-scanpackages -m ~/Documents/GitHub/jesperflodin1.github.io/debs > ~/Documents/GitHub/jesperflodin1.github.io/Packages
+bzip2 -fks ~/Documents/GitHub/jesperflodin1.github.io/Packages
